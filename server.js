34467a5e3ba026b34d0a7dbaa9a9ab4ca9ac7d65
bsen20 <officialbsen@gmail.com>
2025-05-25 17:33:30 +0530

    Added port number from env

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
